5042e5b8ac74e6a5e5a7463c9c1dad6fb7680b4b
presl <presl@COLTONS-DESKTOP>
2026-01-20 20:32:07 -0600

    Add project files.

---
+++ Production Tracker/Form1.Designer.vb
@@ -0,0 +1,63 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        dgvProd = New DataGridView()
+        btnLoad = New Button()
+        CType(dgvProd, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' dgvProd
+        ' 
+        dgvProd.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        dgvProd.Location = New Point(8, 77)
+        dgvProd.Name = "dgvProd"
+        dgvProd.Size = New Size(1347, 779)
+        dgvProd.TabIndex = 0
+        ' 
+        ' btnLoad
+        ' 
+        btnLoad.Location = New Point(1245, 24)
+        btnLoad.Name = "btnLoad"
+        btnLoad.Size = New Size(108, 30)
+        btnLoad.TabIndex = 1
+        btnLoad.Text = "Load Data"
+        btnLoad.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1365, 866)
+        Controls.Add(btnLoad)
+        Controls.Add(dgvProd)
+        Name = "Form1"
+        Text = "Form1"
+        CType(dgvProd, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents dgvProd As DataGridView
+    Friend WithEvents btnLoad As Button
+
+End Class
---
+++ Production Tracker/Form1.vb
@@ -0,0 +1,30 @@
+﻿Imports Microsoft.Data.SqlClient
+Imports System.Data
+
+Public Class Form1
+
+    Private ReadOnly _connString As String =
+        "Server=(localdb)\MSSQLLocalDB;Database=ProdPractice;Trusted_Connection=True;TrustServerCertificate=True;"
+
+    Private Sub btnLoad_Click(sender As Object, e As EventArgs) Handles btnLoad.Click
+        Dim sql As String =
+            "SELECT TOP (5000) " &
+            "Id, ProductionDate, Email, Employee, Permit, Task, Production, HoursWorked, Notes " &
+            "FROM dbo.ProdTable " &
+            "ORDER BY ProductionDate DESC, Employee;"
+
+        Dim dt As New DataTable()
+
+        Using conn As New SqlConnection(_connString)
+            Using cmd As New SqlCommand(sql, conn)
+                Using da As New SqlDataAdapter(cmd)
+                    da.Fill(dt)
+                End Using
+            End Using
+        End Using
+
+        dgvProd.DataSource = dt
+    End Sub
+
+End Class
+
